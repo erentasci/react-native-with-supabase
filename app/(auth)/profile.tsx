c636f12c831687b528bccc1ff538d1b00c0eb4e8
Eren Taşçı <erent4sci@gmail.com>
2025-05-05 23:42:26 +0300

    feat: capture image with expo-image-picker

--- app/(auth)/profile.tsx
+++ app/(auth)/profile.tsx
@@ -1,14 +1,42 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import React, { useState } from "react";
+import { Button, Image, StyleSheet, View } from "react-native";
 
 const Page = () => {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View>
-      <Text>Profile</Text>
+      {image && <Image source={{ uri: image }} style={styles.avatar} />}
+      {!image && <Image style={styles.avatar} />}
+
+      <Button title="Set Avatar Image" onPress={pickImage} />
     </View>
   );
 };
 
 export default Page;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  avatar: {
+    width: 200,
+    height: 200,
+    backgroundColor: "#ccc",
+    alignSelf: "center",
+    borderRadius: 100,
+    margin: 40,
+  },
+});

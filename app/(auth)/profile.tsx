5afc48f7e5fc751e864af1528b387adf7cb8bcee
Eren Taşçı <erent4sci@gmail.com>
2025-05-06 00:10:16 +0300

    feat: uploading to file to supabase

--- app/(auth)/profile.tsx
+++ app/(auth)/profile.tsx
@@ -1,13 +1,43 @@
+import { supabase } from "@/utils/supabase";
+import { decode } from "base64-arraybuffer";
+import * as FileSystem from "expo-file-system";
 import * as ImagePicker from "expo-image-picker";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Image, StyleSheet, View } from "react-native";
 
 const Page = () => {
   const [image, setImage] = useState<string | null>(null);
 
+  useEffect(() => {
+    loadUserAvatar();
+  }, []);
+
+  const loadUserAvatar = async () => {
+    const {
+      data: { user: User },
+    } = await supabase.auth.getUser();
+
+    supabase.storage
+      .from("avatars")
+      .download(`${User?.id}/avatar.png`)
+      .then(({ data }) => {
+        console.log(data);
+        if (!data) return;
+
+        const fr = new FileReader();
+        fr.readAsDataURL(data!);
+        fr.onload = () => {
+          setImage(fr.result as string);
+        };
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: "images",
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
@@ -15,6 +45,25 @@ const Page = () => {
 
     if (!result.canceled) {
       setImage(result.assets[0].uri);
+
+      const {
+        data: { user: User },
+      } = await supabase.auth.getUser();
+
+      const image = result.assets[0];
+
+      const base64 = await FileSystem.readAsStringAsync(image.uri, {
+        encoding: "base64",
+      });
+      const filePath = `${User?.id}/avatar.png`;
+      const contentType = "image/png";
+      try {
+        await supabase.storage
+          .from("avatars")
+          .upload(filePath, decode(base64), { contentType });
+      } catch (error) {
+        console.log("Error uploading image:", error);
+      }
     }
   };
 
